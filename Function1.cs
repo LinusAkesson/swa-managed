0ca177ad5fc8e011525a798f35d2bd9a518f7be9
Linus Åkesson <linus.akesson@sogeti.com>
2025-02-21 10:33:20 +0100

    init

---
+++ Function1.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace swa_managed
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("GetTitle")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
